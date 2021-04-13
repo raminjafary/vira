df1cc32d5fc14ef7e46d1ca4fc64a208f82cbe86
raminjafary <raminjafary1993@gmail.com>
2021-04-14 00:38:03 +0430

    chore: tweek some settings

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,14 +1,8 @@
-declare const isSSR: boolean;
-declare const _nano: any;
-
+declare var isSSR: boolean;
+declare var _nano: any;
 declare namespace JSX {
   interface Element {}
   interface IntrinsicElements {
-    div: any;
-    a: any;
-    br: any;
-    h1: any;
-    button: any;
-    script: any;
+    [element: string]: any;
   }
 }
