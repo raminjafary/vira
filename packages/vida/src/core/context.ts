c00ad811d95c554353e4c028087c632951bacc63
raminjafary <raminjafary1993@gmail.com>
2021-04-21 19:17:14 +0430

    feat: add createContext

---
+++ packages/vida/src/core/context.ts
@@ -0,0 +1,26 @@
+export default function createContext(ctx: any) {
+  let _ctx = ctx;
+  return {
+    Provider(props: any) {
+      if (props.value) {
+        _ctx = props.value;
+        return props.children;
+      }
+    },
+    Consumer(props: any) {
+      return {
+        component: props.children[0](_ctx),
+        props: {
+          ...props,
+          context: _ctx,
+        },
+      };
+    },
+    get() {
+      return _ctx;
+    },
+    set(ctx: any) {
+      _ctx = ctx;
+    },
+  };
+}
