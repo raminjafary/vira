c627c3434cadff13c0bf59faa5fcd57d785f7cc9
raminjafary <raminjafary1993@gmail.com>
2021-04-30 23:19:44 +0430

    feat: add hyperscript tagged markup

--- packages/vida/src/dev/index.tsx
+++ packages/vida/src/dev/index.tsx
@@ -1,10 +1,10 @@
 import Vida, { Component } from "../";
 import SayHi from "./components/SayHi";
+import { jsx } from "../jsx";
 
 function HydrateLazily({ name }: { name: string }) {
   return Vida.lazyHydration(<SayHi name={name} />, null);
 }
-
 class Counter extends Component {
   value = 0;
 
@@ -16,7 +16,7 @@ class Counter extends Component {
   render() {
     return (
       <Vida.Fragment>
-        <div>Counter: {this.value}</div>
+        {jsx`<div>Counter: ${this.value}</div>`}
         <button onClick={() => this.changeValue(1)}>Increment</button>
         <button onClick={() => this.changeValue(-1)}>Decrement</button>
         <div style={{ height: "1000px" }}></div>
---
+++ packages/vida/src/jsx/index.ts
@@ -0,0 +1,5 @@
+import { h } from "../core";
+import htm from "./htm";
+export default htm;
+
+export const jsx = htm.bind(h);
